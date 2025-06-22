426d69bc212f2e7e9b3144a6161d8b143a92df29
moiseeev <m.moiseev@g.nsu.ru>
2025-06-22 17:33:43 +0700

    refactor: create class DbHelper

--- auth_service/app/db/session.py
+++ auth_service/app/db/session.py
@@ -1,25 +1,54 @@
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-from sqlalchemy.orm import sessionmaker
-from typing import AsyncGenerator
+from collections.abc import AsyncGenerator
+
+from pydantic import PostgresDsn
+from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession, async_sessionmaker, create_async_engine
+
 from auth_service.app.settings import settings
 
-engine = create_async_engine(
-    settings.DATABASE_URL,
-    pool_pre_ping=True,
-    echo=settings.DATABASE_ECHO,
-    pool_size=settings.DATABASE_POOL_SIZE,
-    max_overflow=settings.DATABASE_MAX_OVERFLOW,
+
+class PostgreHelper:
+    def __init__(
+        self,
+        url: str,
+        echo: bool = True,
+        future: bool = True,
+        echo_pool: bool = False,
+        pool_size: int = 5,  # максимум 5 постоянных подключений
+        max_overflow: int = 10  # + до 10 временных подключений при пиковых нагрузках
+    ) -> None:
+
+        self.engine: AsyncEngine = create_async_engine(
+            url,
+            echo=echo,
+            future=future,
+            echo_pool=echo_pool,
+            pool_size=pool_size,
+            max_overflow=max_overflow,
+        )
+
+        self.session_factory: async_sessionmaker[AsyncSession] = async_sessionmaker(
+            bind=self.engine,
+            autoflush=False,
+            autocommit=False,
+            expire_on_commit=False
+        )
+
+    async def dispose(self) -> None:
+        await self.engine.dispose()
+
+    async def get_db_session(self) -> AsyncGenerator[AsyncSession, None]:
+        async with self.session_factory() as session:
+            yield session
+
+
+DATABASE_URL = PostgresDsn.build(
+    scheme="postgresql+asyncpg",
+    username=settings.POSTGRES_USER.get_secret_value(),
+    password=settings.POSTGRES_PASSWORD.get_secret_value(),
+    host="localhost",  # settings.POSTGRES_HOST.get_secret_value(), # костыль, тут нужно будет подумать
+    port=settings.POSTGRES_PORT,
+    path=settings.POSTGRES_DB.get_secret_value(),
 )
 
-AsyncDBSession = sessionmaker(
-    bind=engine,
-    class_=AsyncSession,
-    expire_on_commit=False,
-    autoflush=False,
-    autocommit=False,
-)
-
-
-async def get_db_session() -> AsyncGenerator[AsyncSession, None]:
-    async with AsyncDBSession() as session:
-        yield session
+# нужно вынести в settings
+db_helper = PostgreHelper(url=str(DATABASE_URL), echo=True, echo_pool=False, pool_size=10, max_overflow=10)
